d8de9288b9e54f5eb1a94e9a681ef6b3d5e2122a
raptorhawk9 <142059090+raptorhawk9@users.noreply.github.com>
2025-09-06 11:41:48 -0700

    Get rid of template stuff

--- lib/main.dart
+++ lib/main.dart
@@ -88,19 +88,10 @@ class _HomePageState extends State<HomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const Text('Welcome to G2E! Go to a page to access a resource! '),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
