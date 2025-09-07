0320cc16d324431bdf1763fbf5d85b2f1090a3e6
raptorhawk9 <142059090+raptorhawk9@users.noreply.github.com>
2025-09-07 09:52:16 -0700

    Some stuff

---
+++ lib/pages/navigation.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+
+class NavigationPage extends StatefulWidget {
+  @override
+  State<NavigationPage> createState() => _NavigationPageState();
+}
+
+class _NavigationPageState extends State<NavigationPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
--- lib/widgets/g2e_appbar.dart
+++ lib/widgets/g2e_appbar.dart
@@ -7,7 +7,6 @@ class AidlinkAppbar extends StatelessWidget implements PreferredSizeWidget {
   const AidlinkAppbar({super.key, required this.title}); 
 
   void navigateToNewSubpage(BuildContext context, String route) {
-    print("hi");
     Navigator.popUntil(context, ModalRoute.withName("/"));
     Navigator.pushNamed(context, route);
   }
@@ -18,9 +17,9 @@ class AidlinkAppbar extends StatelessWidget implements PreferredSizeWidget {
       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(title),
         actions: [
-          TextButton(onPressed: () => {navigateToNewSubpage(context, '/map')}, child: Text("Map")), 
+          TextButton(onPressed: () => {navigateToNewSubpage(context, '/map')}, child: Text("Disaster Map")), 
           TextButton(onPressed: () => {navigateToNewSubpage(context, '/food-calc')}, child: Text("Food Calc")), 
-          TextButton(onPressed: () => {print("Env Nav")}, child: Text("Environment Nav Guide")), 
+          TextButton(onPressed: () => {print("Env Nav")}, child: Text("Navigation")), 
         ],
     );
   }
